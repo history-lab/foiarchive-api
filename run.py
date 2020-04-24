2bcbd79280770051b31a6774d44d335d1a6be69c
Ben Lis <ben@leismart.com>
2020-04-24 18:29:35 -0400

    Simplified run.py and made it PEP-8 compliant; updated requirements.txt.

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-from flask import Flask
 from api import app, args
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=args.port, debug=args.debug, use_reloader=True)
+    app.run(host='127.0.0.1', port=args.port, debug=args.debug,
+            use_reloader=True)
